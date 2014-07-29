fa2953aa102e880fa32e16d79b292bdb0c7170c0
Andrew Selbie <aselbie@gmail.com>
2014-07-29 11:43:31 -0700

    getElementsByClassName passes spec

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -5,5 +5,18 @@
 
 // But instead we're going to implement it from scratch:
 var getElementsByClassName = function(className){
-  // your code here
+  var elements = [];
+  
+  var find = function(element) {
+    if (element.classList && element.classList.contains(className)) {
+      elements.push(element);
+    }
+    for (var i = 0; i < element.childNodes.length; i++) {
+      find(element.childNodes[i]);
+    };
+  }
+
+  find(document.body)
+
+  return elements;
 };
--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -3,5 +3,41 @@
 
 // but you don't so you're going to write it from scratch:
 var stringifyJSON = function(obj) {
-  // your code goes here
+
+  if (obj === null) {
+      return 'null';
+  }
+
+  if (obj === undefined) {
+    return '';
+  }
+
+  if (typeof(obj) === 'object') {
+    var props = [];
+    var isArray = obj instanceof Array;
+    
+    for (key in obj) {
+      var propKey = isArray ? '' : '"' + key + '":';
+      var propVal = stringifyJSON(obj[key])
+
+      if (propVal) props.push(propKey + propVal);
+    }
+
+    return (isArray ? '[' : '{') + props.join(',') + (isArray ? ']' : '}');
+  }
+
+  if (typeof(obj) === 'string') {
+    return '"' + obj + '"';
+  }
+
+  if (typeof(obj) === 'number') {
+    return '' + obj;
+  }
+
+  if (typeof(obj) === 'function') {
+    return '';
+  }
+
+  return '' + obj;
+
 };
